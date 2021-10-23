b23c48981b46f304d7b0bdc6d6df5da5ffb85658
SIMPLESHAIKH <simpleshaikh2@gmail.com>
2021-10-23 23:24:25 +0100

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Dell Laptop 10th Gen',
+            category: 'Computer',
+            image: '/images/p1.jpg',
+            price: 840,
+            brand: 'Dell',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high performance'
+        },
+        {
+            _id:'2',
+            name: 'MacBook Pro Retina Display',
+            category: 'Computer',
+            image: '/images/p2.jpg',
+            price: 1240,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high performance'
+        },
+        {
+            _id:'3',
+            name: 'MacBook Pro M1 Chip',
+            category: 'Computer',
+            image: '/images/p3.jpg',
+            price: 1350,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high performance'
+        },
+        {
+            _id:'4',
+            name: 'Screen Protector for MacBook',
+            category: 'Accessories',
+            image: '/images/p4.jpg',
+            price: 100,
+            brand: 'Fur',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high performance'
+        },
+        {
+            _id:'5',
+            name: 'Iphone 11 128gig',
+            category: 'Gadget',
+            image: '/images/p5.jpg',
+            price: 820,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high performance'
+        },
+        {
+            _id:'6',
+            name: 'Iphone11 A14 Bionic Chip',
+            category: 'Gadget',
+            image: '/images/p6.jpg',
+            price: 940,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high performance'
+        },
+    ],
+};
+
+export default data;
